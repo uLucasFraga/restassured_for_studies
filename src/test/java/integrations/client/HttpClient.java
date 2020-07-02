43009b9c1d87ddeb87cdb51a62314ac77b86932d
lucas fraga <lucass.fragaa@gmail.com>
2020-07-02 18:30:31 -0300

    add http client

--- src/test/java/integrations/client/HttpClient.java
+++ src/test/java/integrations/client/HttpClient.java
@@ -1,11 +1,9 @@
-package client;
+package integrations.client;
 
 import io.restassured.response.Response;
-import jdk.nashorn.internal.codegen.ApplySpecialization;
 
 import java.util.Map;
 import java.util.Optional;
-import java.util.logging.Logger;
 
 import static io.restassured.RestAssured.given;
 
@@ -18,6 +16,15 @@ public class HttpClient {
 
   public Optional<Response> get(final String baseURI, String path) {
     return getNoHeaders(baseURI, path);
+  }
+
+  public Optional<Response> post(
+      final String baseURI, String path, Map<String, String> headers, String body) {
+    return postRequest(baseURI, path, headers, body);
+  }
+
+  public Optional<Response> delete(final String baseURI, String path, Map<String, String> headers) {
+    return deleteRequest(baseURI, path, headers);
   }
 
   private Optional<Response> getNoHeaders(String baseURI, String path) {
@@ -38,6 +45,47 @@ public class HttpClient {
               .headers(headers)
               .baseUri(baseURI)
               .get(path)
+              .then()
+              .log()
+              .all()
+              .extract()
+              .response();
+      return Optional.of(response);
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+    return Optional.empty();
+  }
+
+  private Optional<Response> postRequest(
+      String baseURI, String path, Map<String, String> headers, String body) {
+    try {
+      Response response =
+          given()
+              .headers(headers)
+              .baseUri(baseURI)
+              .body(body)
+              .post(path)
+              .then()
+              .log()
+              .all()
+              .extract()
+              .response();
+      return Optional.of(response);
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+    return Optional.empty();
+  }
+
+  private Optional<Response> deleteRequest(
+      String baseURI, String path, Map<String, String> headers) {
+    try {
+      Response response =
+          given()
+              .headers(headers)
+              .baseUri(baseURI)
+              .delete(path)
               .then()
               .log()
               .all()
