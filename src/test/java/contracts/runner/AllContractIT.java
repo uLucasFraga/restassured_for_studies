5e34b33f34d9ddff3cfc8f790519aad0706b48ea
lucas fraga <lucass.fragaa@gmail.com>
2020-12-03 15:59:40 -0300

    branch para testar

--- src/test/java/contracts/runner/AllContractIT.java
+++ src/test/java/contracts/runner/AllContractIT.java
@@ -1,11 +1,9 @@
 package contracts.runner;
 
-import contracts.serverest.carts.CartsContractIT;
-import contracts.serverest.products.ProductsContractIT;
 import contracts.serverest.users.UsersContractIT;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 
 @RunWith(Suite.class)
-@Suite.SuiteClasses({UsersContractIT.class, CartsContractIT.class, ProductsContractIT.class})
+@Suite.SuiteClasses({UsersContractIT.class})
 public class AllContractIT {}
